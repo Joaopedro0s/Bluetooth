f0e6dc4a7f5f0f45af28db1ba49ac87e4b212ece
Jaozin <jeipi7007@gmail.com>
2025-10-21 11:15:05 -0300

    Acesso concedido ao bluetooth

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express');
+const cors = require('cors'); // importe o cors
 
 const app = express();
-
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`servidor rodando em http://localhost:${port}`);
-});
+app.use(cors()); // habilita CORS para todas as rotas
+
+app.use(express.json());
 
 app.get('/status', (req, res) => {
-    res.send('Olá, Mundo!');
+  res.send('Servidor está funcionando!');
 });
 
 app.post('/status', (req, res) => {
-    const { bluetooth } = req.body;
-    console.log('Status do Bluetooth:', bluetooth);
-    res.json({recebido:bluetooth});
+  const { bluetooth } = req.body;
+  if (bluetooth) {
+    console.log('Status do Bluetooth recebido:', bluetooth);
+    res.json({ recebido: bluetooth });
+  } else {
+    res.status(400).json({ erro: 'Campo "bluetooth" ausente' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
